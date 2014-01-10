6f19dbab1de4c4f3c930a3abc077f9c84f11ee2d
Alexandre Page <alexgroove24@gmail.com>
2014-01-09 19:50:05 -0500

    ok

--- config.php
+++ config.php
@@ -32,7 +32,7 @@ define('ENVIRONMENT', TEST_PRODUCTION);
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', 'root');
-define('DATABASE', 'MY_DATABASE_NAME');
+define('DATABASE', 'NAME_OF_YOUR_DATABASE');
 
 
 
